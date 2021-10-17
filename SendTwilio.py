004f83b1f21c8e996d1601a0c270f2d293e36486
Roberto <rc.lima.jr@gmail.com>
2021-10-17 15:32:56 -0300

    ok

--- SendTwilio.py
+++ SendTwilio.py
@@ -1,38 +1,34 @@
+from os import error
 from twilio.rest import Client
 from decouple import config
 import logging
 
 
 # Enviando SMS pelo Twilio
-
-
-# smsTwilio('+5521988857740', 'Teste 3')
-
-
 class SendSMS:
 
-    def __init__(self, numero, mensagem) -> None:
-        self.numero = numero
-        self.mensagem = mensagem
+    def __init__(self, numero, mensagem):
+        self.__numero = numero
+        self.__mensagem = mensagem
 
-    def sendTwilio(numero, mensagem):
+    def sendTwilio(self):
         account_sid = config('account_sid')
         auth_token = config('auth_token')
         from_ = config('from_')
 
         client = Client(account_sid, auth_token)
 
-        # client.api.account.messages.create(
-        #     to=numero,
-        #     from_=config('from'),
-        #     body=mensagem,
-        # )
-        logging.info('Mensagem SMS enviada.')
-        print('passei aqui')
-        print(f'{numero} e {mensagem}')
-        print(f'{account_sid} & {auth_token} & {from_}')
+        try:
+            client.api.account.messages.create(
+                to=self.__numero,
+                from_=config('from_'),
+                body=self.__mensagem,
+            )
+        except:
+            logging.error('Problema ao enviar SMS.')
+        else:
+            logging.info('Mensagem SMS enviada.')
 
-    # sendTwilio('+5521988857740', 'Teste 3')
 
-
-SendSMS.sendTwilio('A', 'B')
+sms = SendSMS('', 'Teste')
+sms.sendTwilio()
